01970ef30febd788dad8cb560d05b0f4b493a2ce
Hangyu Feng <hfeng@financeit.io>
2021-08-11 20:48:40 -0400

    .

--- dataset.py
+++ dataset.py
@@ -1,4 +1,5 @@
 from sklearn import preprocessing
+from .moving_avg import sma
 import numpy as np
 
 def preprocess(raw):
@@ -9,24 +10,6 @@ def preprocess(raw):
 
 def normalize(data):
     return preprocessing.MinMaxScaler().fit_transform(data)
-
-def sma(values):
-    """ simple moving average """
-    return np.mean(values)
-
-def ema(values, k=None):
-    """ expoential moving average:
-    k = 2 / (len(values) + 1), can actually set to other numbers
-    ema[i] = closing_price[i] * k + ema[i-1] * (1-k)
-    can be calculated as weighted sum:
-        sum((1-k)**i * v[i]) / sum((1-k)**i), where i = 0...(N-1), v[0] is today's value
-
-    https://en.wikipedia.org/wiki/Moving_average
-    """
-    N = len(values)
-    if k is None: k = 2 / (N+1)
-    weights = np.power(1-k, range(N))
-    return weights.dot(values) / np.sum(weights)
 
 def calc_ohlcv(normal, window_size):
     """ normalized open, high, low, close, volume data """
@@ -42,9 +25,11 @@ def calc_open(data, window_size):
 def dataset(raw, window_size=50):
     data = preprocess(raw)
     normal = normalize(data)
+
     ohlcv = calc_ohlcv(normal, window_size)
     indicators = calc_indicators(ohlcv)
     open_values = calc_open(data, window_size)
     open_normal = calc_open(normal, window_size)
     y_normalizer = preprocessing().MinMaxScaler().fit(open_values)
+
     return ohlcv, indicators, open_normal, open_values, y_normalizer
---
+++ moving_avg.py
@@ -0,0 +1,17 @@
+def sma(values):
+    """ simple moving average """
+    return np.mean(values)
+
+def ema(values, k=None):
+    """ expoential moving average:
+    k = 2 / (len(values) + 1), can actually set to other numbers
+    ema[i] = closing_price[i] * k + ema[i-1] * (1-k)
+    can be calculated as weighted sum:
+        sum((1-k)**i * v[i]) / sum((1-k)**i), where i = 0...(N-1), v[0] is today's value
+
+    https://en.wikipedia.org/wiki/Moving_average
+    """
+    N = len(values)
+    if k is None: k = 2 / (N+1)
+    weights = np.power(1-k, range(N))
+    return weights.dot(values) / np.sum(weights)
