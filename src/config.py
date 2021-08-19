af9f220dc6380d4a6ed67ae58b91d4d941d6a568
Hangyu Feng <hfeng@financeit.io>
2021-08-19 17:04:33 -0400

    download csv

--- src/config.py
+++ src/config.py
@@ -1,5 +1,11 @@
-CREDENTIALS_PATH = "../credentials.json"
-DATA_PATH = "../data"
+import pandas
+from os.path import dirname
+
+ROOT_PATH = dirname(dirname(__file__))
+CREDENTIALS_PATH = f"{ROOT_PATH}/credentials.json"
+DATA_PATH = f"{ROOT_PATH}/data"
+MEGA_COMPANIES_PATH = f"{ROOT_PATH}/data/meta/nasdaq_screener_1629404898388(200B+).csv"
+LARGE_COMPANIES_PATH = f"{ROOT_PATH}/data/meta/nasdaq_screener_1629404871627(10B+).csv"
 
 WIN_SIZE = 50
 MODEL_DEFAULT = {
@@ -10,3 +16,8 @@ MODEL_DEFAULT = {
     'adam_loss': 'mse',
     'window_size': WIN_SIZE
 }
+
+
+MEGA_COMPANIES = list(pandas.read_csv(MEGA_COMPANIES_PATH)['Symbol'])
+LARGE_COMPANIES = list(pandas.read_csv(LARGE_COMPANIES_PATH)['Symbol'])
+SELECTED_COMPANIES = ['AMD', 'IBM', 'MSFT', 'PCG', 'WMT']
--- src/main.py
+++ src/main.py
@@ -1,6 +1,12 @@
 from data_loader import DataLoader
 from model import model
+from config import SELECTED_COMPANIES
 
 if __name__ == "__main__":
 
-    mdl = model()
+    loader = DataLoader()
+
+    # one-timer
+    if False:
+        for comp in SELECTED_COMPANIES:
+            loader.save_timeseries_daily(comp)
